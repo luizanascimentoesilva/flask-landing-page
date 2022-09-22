0d3341016fa0d1e3b727a8ebbec50ba2d6ebb8a0
Your Name <you@example.com>
2022-09-22 20:53:13 -0300

    começo do carrosel

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/')
 def index():
         return render_template('home.html')
